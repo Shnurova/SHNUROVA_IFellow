58c394b4699105b733f804bdd43652f94d399ddd
Ludmila Shnurova <ludo4ka948@inbox.ru>
2024-03-28 10:39:37 +0300

    remove unnecessary import

--- src/test/java/hooks/WebHooks.java
+++ src/test/java/hooks/WebHooks.java
@@ -3,7 +3,6 @@ package hooks;
 import io.cucumber.java.Before;
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
-import steps.RestSteps;
 
 public class WebHooks {
     @Before
